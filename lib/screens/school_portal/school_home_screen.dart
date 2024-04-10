294bb866673d75379f34479a03395d7630852fd9
SyedMahiuddin <syedmahiuddin01@gmail.com>
2024-04-11 02:49:54 +0600

    chadrat

--- lib/screens/school_portal/school_home_screen.dart
+++ lib/screens/school_portal/school_home_screen.dart
@@ -64,6 +64,13 @@ class _SchoolHomeScreenState extends State<SchoolHomeScreen> {
                             color: Colors.white,
                             fontWeight: FontWeight.w500),
                       ),
+                      Text(
+                        "Registration No: 01325458",
+                        style: TextStyle(
+                            fontSize: 16.sp,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500),
+                      ),
                     ],
                   ),
                   Column(
@@ -136,13 +143,7 @@ class _SchoolHomeScreenState extends State<SchoolHomeScreen> {
                             title: 'Programs',
                           ),
                         ),
-                        InkWell(
-                          onTap: () {},
-                          child: const CommonCard(
-                            icon: Icons.photo_library_sharp,
-                            title: 'Registration Details',
-                          ),
-                        ),
+
                       ],
                     ),
                     SizedBox(
